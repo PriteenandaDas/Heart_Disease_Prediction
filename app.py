6c256e8bb3f07f79b52f966b44ce27944a99f323
Priteenanda Das <priteenanda2005@email.com>
2025-10-28 19:45:06 +0530

    Updated app.py to bind to 0.0.0.0

--- app.py
+++ app.py
@@ -35,4 +35,6 @@ def predict():
     return render_template('result.html', prediction_text='{}'.format(output),suggestion_text='{}'.format(suggest))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
